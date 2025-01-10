7cfe01e7565a61804734a2d850c6321757851e62
unknown <randerifurutai@gmail.com>
2025-01-10 10:19:58 -0300

    second commit

--- src/routes/main.ts
+++ src/routes/main.ts
@@ -1,7 +1,13 @@
 import { Router } from 'express';
+import { prisma } from '../libs/prisma';
 
 export const mainRouter = Router();
 
 mainRouter.get('/ping', (req, res) => {
     res.json({ pong: true });
 });
+
+mainRouter.get('/test', (req, res)=>{
+   const users= prisma.user.findMany()
+   res.json(users)
+})
